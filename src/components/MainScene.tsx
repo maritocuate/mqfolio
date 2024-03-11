30b90c792ac2d94b4545341c7dbe6f6fe6c54da8
maritocuate <mario300@gmail.com>
2024-03-11 20:44:04 -0300

    ✨ overlay component

---
+++ src/components/MainScene.tsx
@@ -0,0 +1,18 @@
+import { useRef } from 'react'
+import { Canvas } from '@react-three/fiber'
+import Overlay from './Overlay'
+
+export default function MainScene() {
+  const overlay = useRef<HTMLDivElement | null>(null)
+  const caption = useRef<HTMLSpanElement | null>(null)
+  const scroll = useRef<number>(0)
+
+  return (
+    <>
+      <Canvas shadows eventPrefix="client">
+        <ambientLight intensity={1} />
+      </Canvas>
+      <Overlay ref={overlay} caption={caption} scroll={scroll} />
+    </>
+  )
+}
